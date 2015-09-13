5ed57b0b3c6194a045053d68c4dec7ce1b8100d8
keyshames <rkeydude@gmail.com>
2015-09-12 20:41:31 -0700

    big update

--- functions.js
+++ functions.js
@@ -4,37 +4,41 @@ $(document).ready(function(){
 		total: 0,
 		unit: 1,
 		rate: 1,
+        idle: 0,
+        boost: 0,
 		add : function() {
 			diamond.total += (diamond.unit * diamond.rate);
-		}
+            stats();
+		},
 		timer : function() {
-			idle
+           setInterval(function() {
+               diamond.total += (diamond.unit * diamond.idle);
+               stats();
+           }, 1400);
 		}
 	};
+    diamond.timer();
 	
+    
+    
 
 	var player = {
 		dig : function() {
 			diamond.add();
-			stats();
-		},
-		craft : function() {
-			diamond.timer();
-			stats();
 		},
 		buy : function(i) {
 			if(item[i].cost <= diamond.total){
 				diamond.total -= item[i].cost;
 				item[i].inventory++;
 				switch(item[i].type) {
-					case dig:
+					case "mining":
 						diamond.rate += item[i].rate;
 						break;
-					case craft:
-						diamond.rate += item[i].rate;
+					case "crafting":
+						diamond.idle += item[i].idle;
 						break;
-					case boost:
-						upgrade.rate += item[i].rate;
+					case "upgrade":
+						diamond.boost += item[i].boost;
 						break;
 				}
 				stats();
@@ -45,14 +49,14 @@ $(document).ready(function(){
 				diamond.total += item[i].resale;
 				item[i].inventory--;
 				switch(item[i].type) {
-					case dig:
+					case "mining":
 						diamond.rate -= item[i].rate;
 						break;
-					case craft:
-						diamond.rate -= item[i].rate;
+					case "crafting":
+						diamond.idle -= item[i].idle;
 						break;
-					case boost:
-						upgrade.rate -= item[i].rate;
+					case "upgrade":
+						diamond.boost -= item[i].boost;
 						break;
 				}
 				stats();
@@ -60,16 +64,45 @@ $(document).ready(function(){
 		}
 	};
 	
+    
+    
 	var item = {};
-	item['picaxe'] = {type: dig, inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+	item["picaxe"]          = {type: "mining", inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+    item["torch"]           = {type: "mining", inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+    item["minecart"]        = {type: "mining", inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+    item["rail"]            = {type: "mining", inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+    item["tnt"]             = {type: "mining", inventory: 0, cost: 2, rate: 0.5, resale: 0.5};
+    item["table"]           = {type: "crafting", inventory: 0, cost: 2, idle: 0.5, resale: 0.5};
+    item["bucket"]          = {type: "crafting", inventory: 0, cost: 2, idle: 0.5, resale: 0.5};
+    item["anvil"]           = {type: "crafting", inventory: 0, cost: 2, idle: 0.5, resale: 0.5};
+    item["hopper"]          = {type: "crafting", inventory: 0, cost: 2, idle: 0.5, resale: 0.5};
+    item["furnace"]         = {type: "crafting", inventory: 0, cost: 2, idle: 0.5, resale: 0.5};
+    item["chest"]           = {type: "upgrade", inventory: 0, cost: 2, boost: 0.5, resale: 0.5};
+    item["bed"]             = {type: "upgrade", inventory: 0, cost: 2, boost: 0.5, resale: 0.5};
+    item["boots"]           = {type: "upgrade", inventory: 0, cost: 2, boost: 0.5, resale: 0.5};
+    item["speed potion"]    = {type: "upgrade", inventory: 0, cost: 2, boost: 0.5, resale: 0.5};
+    item["fire charge"]     = {type: "upgrade", inventory: 0, cost: 2, boost: 0.5, resale: 0.5};
+    
+    
+    
+    
+    
+    
+    
 
 	function stats() {
-		$("#diamond").html(diamond.total);
+		$("#total").html(diamond.total);
 		$("#rate").html(diamond.rate);
-		$("#inventory").html(item['picaxe'].inventory);
+        $("#idle").html(diamond.idle);
+		$("#inventory").html(item["picaxe"].inventory);
 	}
 
-	$("#dig").on("click", function() {
+    
+    
+    
+    
+    
+	$("#big-diamond").on("click", function() {
 		player.dig();
 	});
 	$(".buy").on("click", function() {
